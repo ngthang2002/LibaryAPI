7c782b8387b2f468c72e709f9f92c38029699c85
ngthang2002 <ngthang200201@gmail.com>
2023-10-05 13:05:37 +0700

    api

--- DocGia.php
+++ DocGia.php
@@ -1,28 +1,35 @@
 <?php
-    include "connection.php";
-    $method = $_SERVER['REQUEST_METHOD'];
-    header('Content-Type: application/json');
-    switch ($method) {
+include "connection.php";
+$method = $_SERVER['REQUEST_METHOD'];
+header('Content-Type: application/json');
+switch ($method) {
     case 'GET':
+        if (isset($_GET['TenDG']))
+            $sql = "Select * from docgia where TenDG LIKE '%{$_GET['TenDG']}%'";
+        else
+            if (isset($_GET['MaDG']))
+            $sql = "Select * from docgia where MaDG = '{$_GET['MaDG']}'";
+        else
+            $sql = "Select * from docgia";
         // Xử lý yêu cầu GET để lấy danh sách người dùng
-        $result =  mysqli_query($conn, "Select * from docgia");
-        $users= array();
-        while ($row = mysqli_fetch_assoc($result)){
+        $result =  mysqli_query($conn, $sql);
+        $users = array();
+        while ($row = mysqli_fetch_assoc($result)) {
             $users[] = $row;
         }
         echo json_encode($users);
         break;
     case 'POST':
         // Xử lý yêu cầu POST để thêm người dùng mới
-        $data = json_decode(file_get_contents('php://input'), true);    
+        $data = json_decode(file_get_contents('php://input'), true);
 
         // Xác thực dữ liệu (trong thực tế, bạn nên kiểm tra xác thực mạnh mẽ hơn)
         if (!isset($data['MaDG']) || !isset($data['TenDG']) || !isset($data['NgaySinh']) || !isset($data['GioiTinh'])) {
-            http_response_code(400); // Bad Request
+            http_response_code(201); // Bad Request
             echo json_encode(array('error' => 'Dữ liệu không hợp lệ.'));
             exit;
         }
-        
+
         $query = "insert into docgia values('{$data['MaDG']}', '{$data['TenDG']}', '{$data['NgaySinh']}',
             '{$data['GioiTinh']}', '{$data['DienThoai']}', '{$data['Email']}', '{$data['HanDung']}')";
         $result = mysqli_query($conn, $query);
@@ -40,7 +47,7 @@
             echo json_encode(array('error' => 'Dữ liệu không hợp lệ.'));
             exit;
         }
-        
+
         $query = "Update docgia Set TenDG = '{$data['TenDG']}', NgaySinh = '{$data['NgaySinh']}',
             GioiTinh = '{$data['GioiTinh']}', DienThoai = '{$data['DienThoai']}',
              Email = '{$data['Email']}', HanDung = '{$data['HanDung']}' Where MaDG = '{$data['MaDG']}'";
@@ -50,18 +57,13 @@
         echo json_encode(array('message' => 'Thông tin người dùng đã được cập nhật.'));
         break;
     case 'DELETE':
-        // Xử lý yêu cầu DELETE để xóa người dùng
-        $data = json_decode(file_get_contents('php://input'), true);
-
-        // Xác thực dữ liệu (trong thực tế, bạn nên kiểm tra xác thực mạnh mẽ hơn)
-        if (!isset($data['MaDG'])) {
+        if (!isset($_GET['MaDG'])) {
             http_response_code(400); // Bad Request
             echo json_encode(array('error' => 'Dữ liệu không hợp lệ.'));
-            exit;
+            break;
         }
-
         // Xóa người dùng từ cơ sở dữ liệu
-        $query = "DELETE FROM docgia WHERE MaDG = '{$data['MaDG']}'";
+        $query = "DELETE FROM docgia WHERE MaDG = '{$_GET['MaDG']}'";
         $result = mysqli_query($conn, $query);
 
         http_response_code(204); // No Content
--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "libary";
+    $host = "192.168.249.134";
+    $username = "ngt";
+    $password = "Qscefb123..";
+    $database = "libaryutt";
     $conn = mysqli_connect($host,$username,$password,$database);
     if ($conn->connect_error) {
         die("Kết nối đến cơ sở dữ liệu thất bại: " . $conn->connect_error);
